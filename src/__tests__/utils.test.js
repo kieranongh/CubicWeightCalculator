8a5746876f83ee1600744d4c8fd7d209f76db9f5
Kieran L <none>
2020-02-23 00:03:55 +1100

    Mock axios and test API calls

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,4 +1,7 @@
-import { calcCubicWeight } from '../utils'
+import axios from 'axios';
+import { calcCubicWeight, fetchProducts } from '../utils'
+
+jest.mock('axios')
 
 test('Check calcCubicWeight, integer input/output', () => {
   const height = 20,
@@ -32,5 +35,54 @@ test('Check calcCubicWeight, decimal input/output, no floating point errors', ()
   const result = calcCubicWeight({ height, width, length})
 
   expect(result).toEqual(expected)      
+})
+
+test('Fetches API, calculates data, one endpoint', async () => {
+  const base = "base",
+        url = "url",
+        category = "Box",
+        objects = [{
+          title: "Product 1",
+          category: "Box",
+          size: {
+            height: 20,
+            width: 30,
+            length: 40
+          }
+        },
+        {
+          title: "Product 2",
+          category: "Bowl",
+          size: {
+            height: 20,
+            width: 30,
+            length: 40
+          }
+        },
+        {
+          title: "Product 3",
+          category: "Box",
+          size: {
+            height: 20,
+            width: 30,
+            length: 40
+          }
+        }
+      ],
+      next = null
   
+  const resp = { data: { objects, next } }
+  axios.get.mockResolvedValue(resp)
+  const result = await fetchProducts(base, url, category)
+
+  // Category filters as intended
+  expect(result).toHaveLength(2)
+
+  // Order is preserved
+  expect(result[0].title).toBe(objects[0].title)
+
+  // Cubic weight calculated correctly
+  expect(result[0].cubicWeight).toBe(6)
+  
+  expect(axios.get.mock.calls.length).toBe(1)
 })
