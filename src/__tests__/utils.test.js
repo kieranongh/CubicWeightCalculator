375bea5cd5783bd45b0f5d1f10b280aef1944897
Kieran L <none>
2020-02-23 00:28:07 +1100

    Test multiple pages on endpoint

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -37,8 +37,8 @@ test('Check calcCubicWeight, decimal input/output, no floating point errors', ()
   expect(result).toEqual(expected)      
 })
 
-test('Fetches API, calculates data, one endpoint', async () => {
-  const base = "base",
+test('Fetches API, calculates data, one page', async () => {
+  const base = "base/",
         url = "url",
         category = "Box",
         objects = [{
@@ -85,4 +85,89 @@ test('Fetches API, calculates data, one endpoint', async () => {
   expect(result[0].cubicWeight).toBe(6)
   
   expect(axios.get.mock.calls.length).toBe(1)
+})
+
+test('Fetches API, calculates data, two pages', async () => {
+  axios.get.mockClear()
+
+  const base = "base/",
+        url = "url1",
+        category = "Box",
+        objects1 = [{
+          title: "Product 1",
+          category: "Box",
+          size: {
+            height: 20,
+            width: 30,
+            length: 40
+          }
+        },
+        {
+          title: "Product 2",
+          category: "Bowl",
+          size: {
+            height: 20,
+            width: 30,
+            length: 40
+          }
+        },
+        {
+          title: "Product 3",
+          category: "Box",
+          size: {
+            height: 20,
+            width: 30,
+            length: 40
+          }
+        }
+      ],
+      next1 = "url2",
+      objects2 = [{
+        title: "Product 4",
+        category: "Box",
+        size: {
+          height: 20,
+          width: 30,
+          length: 40
+        }
+      },
+      {
+        title: "Product 5",
+        category: "Bowl",
+        size: {
+          height: 20,
+          width: 30,
+          length: 40
+        }
+      },
+      {
+        title: "Product 6",
+        category: "Box",
+        size: {
+          height: 20,
+          width: 30,
+          length: 40
+        }
+      }
+    ],
+    next2 = null
+  
+  const resp1 = { data: { objects: objects1, next: next1 } }
+  const resp2 = { data: { objects: objects2, next: next2 } }
+  axios.get.mockImplementationOnce(() => Promise.resolve(resp1))
+  axios.get.mockImplementationOnce(() => Promise.resolve(resp2))
+  const result = await fetchProducts(base, url, category)
+
+  // Category filters as intended
+  expect(result).toHaveLength(4)
+
+  // Order is preserved
+  expect(result[3].title).toBe(objects2[2].title)
+
+  // Cubic weight calculated correctly
+  expect(result[3].cubicWeight).toBe(6)
+  
+  expect(axios.get.mock.calls.length).toBe(2)
+  expect(axios.get).toHaveBeenCalledWith(base + url)
+  expect(axios.get).toHaveBeenCalledWith(base + next1)
 })
